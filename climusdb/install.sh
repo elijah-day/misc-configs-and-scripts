201a193aae6dd0ab88ccc55ad0e2062388a8d436
Elijah Day <>
2025-09-21 19:20:41 -0500

    Add script for recording files to database with checksums

---
+++ climusdb/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp climusdb-add /usr/local/bin/climusdb-add
+cp climusdb-verify /usr/local/bin/climusdb-verify
+chmod +x /usr/local/bin/climusdb-add
+chmod +x /usr/local/bin/climusdb-verify
+
